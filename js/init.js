0b3b1808113d60ef555b7d2e0410b9b849113265
melissayauri <yaurimelissa@gmail.com>
2018-05-10 18:49:22 -0500

    incluyendo vista works

--- js/init.js
+++ js/init.js
@@ -1,16 +1,18 @@
 
-  $(document).ready(function(){
-    $('.slider').slider();
-    $('.sidenav').sidenav();
+$(document).ready(function() {
+  /* Inicializando el slider*/
+  $('.slider').slider();
+  /* Inicializando el navbar en mobile*/
+  $('.sidenav').sidenav();
 
-     // Initialize Firebase
-     var config = {
-      apiKey: "AIzaSyDdXkiM8c_LdEJVT6OeyEIfOQ2N7k0sjRU",
-      authDomain: "seleccion-gmd.firebaseapp.com",
-      databaseURL: "https://seleccion-gmd.firebaseio.com",
-      projectId: "seleccion-gmd",
-      storageBucket: "",
-      messagingSenderId: "246386059278"
+  // Initialize Firebase
+  var config = {
+    apiKey: 'AIzaSyDdXkiM8c_LdEJVT6OeyEIfOQ2N7k0sjRU',
+    authDomain: 'seleccion-gmd.firebaseapp.com',
+    databaseURL: 'https://seleccion-gmd.firebaseio.com',
+    projectId: 'seleccion-gmd',
+    storageBucket: '',
+    messagingSenderId: '246386059278'
   };
   firebase.initializeApp(config);
-  });
+});
